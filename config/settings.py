13d5755b454c21729cd3e5a5f5659c467939fcd7
Blazej Bielski <bbielski@lyck.pl>
2022-10-25 18:10:38 +0200

    adding settings configuration for application home and users, adding homev application

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users.apps.UsersConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
